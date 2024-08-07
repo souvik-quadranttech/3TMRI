b9dfd4f673bd656e660b92f1984d44d1af2eead7
souvik-quadranttech <souvik@quadranttech.co>
2024-08-07 18:36:09 +0530

    new updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,10 +75,18 @@ const swiper = new Swiper(".card-swiper", {
   },
   breakpoints: {
     640: {
-      slidesPerView: 2,
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    980: {
+      slidesPerView: 1,
       spaceBetween: 20,
     },
     1024: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    1280: {
       slidesPerView: 1.5,
       spaceBetween: 20,
     },
@@ -107,6 +115,26 @@ if (facilitySection) {
   let slideMain = facilitySection.querySelector(".slide-main");
   let rightPos = slideMain.getBoundingClientRect().right;
   let swiperDiv = facilitySection.querySelector(".facility-swiper-div");
-  swiperDiv.style.marginLeft = `${Number(rightPos.toFixed()) + 40}px`;
+  swiperDiv.style.marginLeft = `${Number(rightPos.toFixed()) + 20}px`;
   console.log(rightPos);
 }
+
+// Button Hover
+
+let btn1 = document.querySelectorAll(".btn-primary");
+
+btn1.forEach((btn) => {
+  let span = btn.querySelector("span");
+  let span2 = span.cloneNode(true);
+  span2.classList.add("text2");
+  btn.append(span2);
+});
+
+let btnLight = document.querySelectorAll(".btn-light");
+
+btnLight.forEach((btn) => {
+  let image = btn.querySelector("img");
+  let image2 = image.cloneNode(true);
+  image2.classList.add("arrow-2")
+  btn.append(image2);
+});
